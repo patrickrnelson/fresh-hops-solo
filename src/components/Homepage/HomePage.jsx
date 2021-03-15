39ba6c5d0efe150939b16e6219c5c701b22aeab8
patrickrnelson <patrickrnelson1@gmail.com>
2021-03-15 15:00:03 -0500

    added logout button to test circular user ability

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -1,8 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 function HomePage() {
+  const dispatch = useDispatch();
+  const user = useSelector(store => store.user)
+
+  const handleLogout = () => {
+    dispatch ({
+      type: 'LOGOUT'
+    })
+  }
 
   return (
     <div>
-      <h2>Welcome, Patrick!</h2>
+      <h2>Welcome, {user.name}!</h2>
+      <button onClick={handleLogout}>Log Out</button>
     </div>
   )
 }
