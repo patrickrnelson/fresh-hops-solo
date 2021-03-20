bb039819043cfbf18c28fe49b173851c65ba6684
patrickrnelson <patrickrnelson1@gmail.com>
2021-03-20 18:24:23 -0500

    moved liked and tried status' into own component

--- src/components/BeerDetails/BeerDetails.jsx
+++ src/components/BeerDetails/BeerDetails.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import Header from '../Header/Header'
+import Header from '../Header/Header';
+import StatusChangeInputs from '../StatusChangeInputs';
 
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import Button from '@material-ui/core/Button';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Grid from '@material-ui/core/Grid';
+
 import IconButton from '@material-ui/core/IconButton';
 import Switch from '@material-ui/core/Switch';
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-import ThumbDownAltIcon from '@material-ui/icons/ThumbDownAlt';
+
 
 function BeerDetails() {
   const history = useHistory();
@@ -24,16 +24,13 @@ function BeerDetails() {
 
   // defines the current like status and button colors
   const [likeStatus, setLikeStatus] = useState(beerDetails[0].is_liked)
-  const [thumbsUpColor, setThumbsUpColor] = useState(beerDetails[0].is_liked ? 'primary' : 'inherit')
-  const [thumbsDownColor, setThumbsDownColor] = useState(beerDetails[0].is_liked === false ? 'secondary' : 'inherit')
 
   // render additional will render the 'like status', 'tried status', 
   // and 'save changes' button when true
   const [renderAdditional, setRenderAdditional] = useState(false);
   // defines the current tried status and button colors
   const [triedStatus, setTriedStatus] = useState(false);
-  const [haveTriedColor, setHaveTriedColor] = useState(beerDetails[0].has_tried ? 'primary' : 'inherit');
-  const [notTriedColor, setNotTriedColor] = useState(beerDetails[0].has_tried === false ? 'secondary' : 'inherit');
+  
 
   const [saveChangesColor, setSaveChangesColor] = useState('inherit');
   const [saveChangesBtnVariant, setSaveChangesBtnVariant] = useState('inherit');
@@ -73,83 +70,9 @@ function BeerDetails() {
     
   } // end buttonClick
 
-  const changeTriedStatus = () => {
-    setTriedStatus(!triedStatus)
-  }
-
-  // handle tried status changes and colors of inputs
-  const triedClick = () => {
-    if(triedStatus === null || triedStatus === false) {
-      setTriedStatus(true);
-      setHaveTriedColor('primary')
-      setNotTriedColor('inherit')
-    }
-    else {
-      // reset like status and thumb colors
-      setLikeStatus(null);
-      setThumbsUpColor('inherit')
-      setThumbsDownColor('inherit')
-      // set the correct tries status and colors
-      setTriedStatus(null)
-      setHaveTriedColor('inherit')
-      setNotTriedColor('inherit')
-    }
-    // if anything changes, change the save changes button to indicate that the user needs to save
-    setSaveChangesColor('primary')
-    setSaveChangesBtnVariant('contained')
-  } // end triedClick
-
-  // handle tried status changes and colors of inputs
-  const notTriedClick = () => {
-    if(triedStatus === null || triedStatus === true) {
-      // reset like status and thumb colors
-      setLikeStatus(null);
-      setThumbsUpColor('inherit')
-      setThumbsDownColor('inherit')
-      // set the correct tries status and colors
-      setTriedStatus(false);
-      setHaveTriedColor('inherit')
-      setNotTriedColor('secondary')
-    }
-    else {
-      setTriedStatus(null)
-      setHaveTriedColor('inherit')
-      setNotTriedColor('inherit')
-    }
-    // if anything changes, change the save changes button to indicate that the user needs to save
-    setSaveChangesColor('primary')
-    setSaveChangesBtnVariant('contained')
-  } // end notTriedClick
-
-  const likeClick = () => {
-    if(likeStatus === null || likeStatus === false) {
-      setLikeStatus(true)
-      setThumbsUpColor('primary')
-      setThumbsDownColor('inherit')
-    } else {
-      setLikeStatus(null)
-      setThumbsUpColor('inherit')
-      setThumbsDownColor('inherit')
-    }
-    // if anything changes, change the save changes button to indicate that the user needs to save
-    setSaveChangesColor('primary')
-    setSaveChangesBtnVariant('contained')
-  } // end likeClick
-
-  const dislikeClick = () => {
-    if(likeStatus === null || likeStatus === true) {
-      setLikeStatus(false)
-      setThumbsUpColor('inherit')
-      setThumbsDownColor('secondary')
-    } else {
-      setLikeStatus(null)
-      setThumbsUpColor('inherit')
-      setThumbsDownColor('inherit')
-    }
-    // if anything changes, change the save changes button to indicate that the user needs to save
-    setSaveChangesColor('primary')
-    setSaveChangesBtnVariant('contained')
-  } // end dislikeClick
+  // const changeTriedStatus = () => {
+  //   setTriedStatus(!triedStatus)
+  // }
 
   // Will update beer status' in the DB if they change on this page
   const updateBeer = () => {
@@ -191,32 +114,9 @@ function BeerDetails() {
       </div>
         {/* Tried Status Input */}
         {renderAdditional ? 
-        <Grid container spacing={3} alignItems='center' justify='center' style={{marginTop: '35px'}}>
-        <Grid container direction="column"  alignContent="center" style={{width: '60%'}}>
-          <Grid item>
-            <p>Have you drank this beer?</p>
-            <div style={{display: 'flex', justifyContent: 'space-around'}}>
-              <Button onClick={notTriedClick} color={notTriedColor}>NO</Button>
-              <Button onClick={triedClick} color={haveTriedColor}>YES</Button>
-            </div>
-          </Grid>
-          {triedStatus ?
-          <Grid item>
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-              <p>Did you like this beer?</p>
-            </div>
-            <div style={{display: 'flex', justifyContent: 'space-around'}}>
-              <IconButton onClick={dislikeClick}>
-                <ThumbDownAltIcon color={thumbsDownColor}/>
-              </IconButton>
-              <IconButton onClick={likeClick}>
-                <ThumbUpAltIcon color={thumbsUpColor}/>
-              </IconButton>
-            </div>
-          </Grid>
-          : <div></div>}
-        </Grid>
-        </Grid>
+          <StatusChangeInputs 
+            likeStatus={likeStatus}
+            setLikeStatus={setLikeStatus} />
         : <div></div>
         }
         
