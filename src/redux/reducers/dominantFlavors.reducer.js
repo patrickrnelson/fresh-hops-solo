43dcf4c5458e0de50bcc3250171c83e8d69f7cee
patrickrnelson <patrickrnelson1@gmail.com>
2021-03-29 16:20:19 -0500

    changed the dominant flavors reducer

--- src/redux/reducers/dominantFlavors.reducer.js
+++ src/redux/reducers/dominantFlavors.reducer.js
@@ -1,16 +1,16 @@
 const dominantFlavorsReducer = (state = [
-  {id: 1, flavor_name: 'malty'},
-  {id: 2, flavor_name: 'bitter'},
-  {id: 3, flavor_name: 'balanced'},
-  {id: 4, flavor_name: 'hoppy'},
-  {id: 5, flavor_name: 'roasty'},
-  {id: 6, flavor_name: 'sweet'},
-  {id: 7, flavor_name: 'smoke'},
-  {id: 8, flavor_name: 'sour'},
-  {id: 9, flavor_name: 'wood'},
-  {id: 10, flavor_name: 'fruit'},
-  {id: 11, flavor_name: 'spice'},
-  {id: 12, flavor_name: 'juicy'},
+  {id: 1, flavor_name: 'Malty'},
+  {id: 2, flavor_name: 'Bitter'},
+  {id: 3, flavor_name: 'Balanced'},
+  {id: 4, flavor_name: 'Hoppy'},
+  {id: 5, flavor_name: 'Roasty'},
+  {id: 6, flavor_name: 'Sweet'},
+  {id: 7, flavor_name: 'Smoke'},
+  {id: 8, flavor_name: 'Sour'},
+  {id: 9, flavor_name: 'Wood'},
+  {id: 10, flavor_name: 'Fruit'},
+  {id: 11, flavor_name: 'Spice'},
+  {id: 12, flavor_name: 'Juicy'},
 ], action) => {
       return state;
   };
